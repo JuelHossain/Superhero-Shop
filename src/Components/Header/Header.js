b6c30db7e94966f1e8e46f7ed6f7db95a3fa038c
juelhossain <juelhossainnn@gmail.com>
2022-04-28 21:58:42 +0600

    header done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <h1>Be A Hero</h1>
+                <p>So You want to be a hero. Choose a Hero from the Product list, Purchase it and get the super power mention in the product description</p>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
